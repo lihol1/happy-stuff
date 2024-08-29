14718a8a3409776a9cf38bca360cb0f108ed6ffd
lihol1 <lihol1@mail.ru>
2024-08-29 12:03:28 +0300

    corrected and replaced manifest

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,10 @@
 import { Inter } from "next/font/google";
 import "./globals.scss";
 import Provider from './StoreProvider';
-// import { Providers } from '../components/Providers';
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
 import Sidebar from '@/components/Sidebar/Sidebar';
 import UserForm from "@/components/User/UserForm";
-// import Head from 'next/head'
-// import LStorage from '../utils/Storage'
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -41,7 +38,7 @@ export const metadata = {
     images: [
       
       {
-        url: 'https://happy-stuff.vercel.app/images/og-image.webp', // Must be an absolute URL //заменить
+        url: 'https://happy-stuff.vercel.app/images/og-image.webp', // Must be an absolute URL 
         width: 1200,
         height: 630,
         alt: 'A man and a woman are on a beach',
@@ -64,48 +61,18 @@ export const metadata = {
     //   'max-snippet': -1,
     },
   },
-  // icons:
-
-  //     [
-  //       {
-  //         // rel: "icon",
-  //       url:'/icon.ico?v=1',
-  //       sizes: 'any',
-  //       type: 'image/x-icon'
-  //     },
-  //     {
-  //       // rel: "icon",
-  //       url:'/iconfav.svg',       
-  //       type: 'image/svg+xml'
-  //     },
-  //     {
-  //       url:'/apple-icon.png?v=4',       
-  //       type: 'image/png'
-  //     }
-  //   ]
-    // ,
-    // apple: ['/apple-touch-icon.png?v=4'],    
-
-  // manifest: '/site.webmanifest'  //Должна быть абсолютная ссылка?
+  
 };
 
 
-// const userState = useSelector(state => state.user)
-// console.log(userState)
+
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">   
-      {/* <Head>
-        <link rel="icon" href="/public/favicon.ico" sizes="48x48" />
-        <link rel="icon" href="/public/icon.svg" type="image/svg+xml" /> 
-        <link rel="apple-touch-icon" sizes="180x180" href="/public/apple-touch-icon.png" />    
-        <link rel="manifest" href="/public/site.webmanifest" />   
-      </Head>   */}
-      <body className={inter.className}> 
-        {/* <Providers> */}
-          <Provider>
-            {/* <LStorage> */}
+      
+      <body className={inter.className}>        
+          <Provider>           
             <div className="wrapper">
               <Header />
               
@@ -120,10 +87,9 @@ export default function RootLayout({ children }) {
               </div>
 
               <Footer />
-            </div>
-            {/* </LStorage> */}
+            </div>           
           </Provider>
-        {/* </Providers>     */}
+        
       </body>
     </html>
   );
