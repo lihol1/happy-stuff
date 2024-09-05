614ca6bf807a5aa9ef1c6f7d05a98131c0dfc57e
lihol1 <lihol1@mail.ru>
2024-09-05 12:32:22 +0300

    feat: added button Up

--- src/components/UpBtn/UpBtn.jsx
+++ src/components/UpBtn/UpBtn.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
+"use client"
+import { useEffect, useState } from "react";
+import { FaChevronUp } from "react-icons/fa6";
 
 const UpBtn = () => {
+    const [isVisible, setIsVisible] = useState(false)
+   
+    useEffect(() => {        
+        const toggleVisibility = () => { 
+            const userAgent = window.navigator.userAgent;
+            // let type = /Android|iPhone|iPad|IEMobile/i.test(userAgent) ? 'mobile' : 'desktop';
+
+            //исключаем Яндекс браузер на мобильных устройствах
+            if ((userAgent.search(/YaApp/) < 0) || (userAgent.search(/YaSearchBrowser/) < 0)) {
+                window.scrollY > 1950 ? setIsVisible(true) : setIsVisible(false)  
+            }        
+        }
+    
+        window.addEventListener("scroll", toggleVisibility)
+        
+        return () => {
+        window.removeEventListener("scroll", toggleVisibility)
+        }
+    }, [])
+
+ 
+    function scrollToTop (){
+        isVisible &&
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        })  
+    };
+
     return (
-        <div>
-            
-        </div>
-    );
+        <button className={isVisible? "upbtn" : "upbtn hidden"} onClick={scrollToTop} >
+          <FaChevronUp />
+        </button>
+      )
+
 };
 
 export default UpBtn;
